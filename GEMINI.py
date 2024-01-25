7def68a73130c0f36bd8d0d91dac60945c7fc496
CrisU8 <cristopher.urbina.h@ug.uchile.cl>
2024-01-25 02:08:02 -0300

    gemini

--- GEMINI.py
+++ GEMINI.py
@@ -4,8 +4,8 @@ import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-#genai.configure(api_key=st.secrets["API_KEY"])
+#genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["API_KEY"])
 
 generation_config = {
   "temperature": 0.0,
